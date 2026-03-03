907038053bca1109f0c1ea6c8817639000a91e98
sadaqsam <sadaqsamor99@gmail.com>
2026-03-03 14:33:52 +0300

    some navigation

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -6,39 +6,42 @@ const Nav = () => {
     const navLinks = [
         {
             name:"Home",
-            icon: <HomeIcon/>,
+            icon: <HomeIcon size={20}/>,
             path: "/"
         } ,
         {
             name:"About",
-            icon: <User2/>,
+            icon: <User2 size={20}/>,
             path: "/"
         }   ,
         {
             name:"Resume",
-            icon: <IdCard/>,
+            icon: <IdCard size={20}/>,
             path: "/"
         }   ,
         {
             name:"Contact-me",
-            icon: <Contact2Icon/>,
+            icon: <Contact2Icon size={20}/>,
             path: "/"
         }   
     ]
   return (
-    <div className='flex justify-between font-bold px-3 py-2 bg-gray-700'>
+    <div className='flex justify-between font-bold px-4 items-center bg-gray-700 text-white py-4'>
         <h1>samo</h1>
-        <div className='flex space-x-4'>
+        <div className='flex space-x-2'>
             {
                 navLinks.map((link)=>(
-                    <NavLink to={link.path} className='flex gap-4'>
+                    <NavLink to={link.path} className='flex gap-1 text-sm'>
                         <span>{link.icon}</span>
                         {link.name}
                     </NavLink>
                 ))
             }
         </div>
-        
+
+        <button className='text-white bg-blue-600 px-2 py-1 rounded'>
+            Download Cv
+        </button>
     </div>
   )
 }
