14a04c02512103906e2a19069b03927ed03b11f0
sadaqsam <sadaqsamor99@gmail.com>
2026-03-03 15:21:48 +0300

    created home and about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Nav from './Components/Nav'
 import Main from './Components/Main'
+import { Routes } from 'react-router-dom'
+import { Route } from 'lucide-react'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
     <div>
       <Nav/>
-      <Main/>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ client/src/Pages/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        About Me
+
+I’m a Full-Stack Software Engineer specializing in the MERN stack (MongoDB, Express.js, React, and Node.js). I build scalable, secure, and high-performance web applications with a strong focus on clean code, usability, and real-world problem solving.
+
+I enjoy working across the full development lifecycle — from designing intuitive user interfaces to developing robust backend systems and APIs. I pay close attention to performance, security, and maintainability, ensuring that every solution I build is reliable and future-ready.
+
+What I Do
+
+Build modern, responsive web applications
+
+Develop RESTful APIs and backend services
+
+Design and optimize databases
+
+Implement authentication & authorization systems
+
+Collaborate, debug, and continuously improve systems
+
+My Approach
+
+I believe great software is built through clarity, consistency, and continuous learning. I’m always exploring new technologies, improving my skills, and writing code that is easy to understand, test, and scale.
+    </div>
+  )
+}
+
+export default About
