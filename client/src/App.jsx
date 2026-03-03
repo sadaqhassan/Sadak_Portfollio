edc7749e359a476537fd62c0dc2c2317db15d5b1
sadaqsam <sadaqsamor99@gmail.com>
2026-03-03 15:12:10 +0300

    completed main banner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './Components/Nav'
+import Main from './Components/Main'
 
 const App = () => {
   return (
     <div>
       <Nav/>
+      <Main/>
     </div>
   )
 }
--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='flex flex-col md:flex-row justify-center items-center px-4 gap-4 mt-40  w-full mx-auto overflow-x-hidden'>
+        <img className='w-60 h-60 rounded-full' src="/img.png" alt="" />
+        <div className='flex flex-col gap-2'>
+            <span className='font-bold text-4xl'>Hello , I'm Sadak Hassan</span>
+            <p className='font-medium text-2xl border-t'> Full-Stack Software Engineer | MERN Stack</p>
+            <p className='text-sm'>Full-Stack Software Engineer specializing in the MERN stack (MongoDB, Express.js, React, Node.js) <br/>  with a strong focus on building scalable, secure, and high-performance web applications. Experienced in  <br/> developing both frontend and backend systems, creating clean, maintainable code,<br/> and designing RESTful APIs.</p>
+  
+        </div>
+    </div>
   )
 }
 
